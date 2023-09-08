2aecb0408d19963b3b9ed6d405d29f8d0f2ffe12
Abhay Kumar <abhaykumar01234@gmail.com>
2023-09-09 04:14:10 +0530

    feat: first commit

---
+++ src/Layout.tsx
@@ -0,0 +1,9 @@
+export const Layout = () => {
+  return (
+    <div className="layout">
+      <header>Header</header>
+      <main className="page">App Page</main>
+      <footer>Footer</footer>
+    </div>
+  );
+};
